b04bb00ba9c1cfeaa412e44192f33ba452c0cb03
Descy93 <74730041+Descy93@users.noreply.github.com>
2020-11-30 19:17:22 +0100

    Updated time js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+//Time function
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours= date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
 //general API
 function displayTemperature(response){
     console.log(response.data.main.temp);
@@ -6,47 +31,22 @@ function displayTemperature(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#speed");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML =  response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     wind.innerHTML = Math.round(response.data.wind.speed);
-
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(
-      response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);  
 }
+
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 let url = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${apiKey}&units=${globalUnits}`;
 axios.get(apiUrl).then(displayWeatherCondition);
 
-//date format
-function formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    let dayIndex = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[dayIndex];
-  
-    return `${day} ${hours}:${minutes}`;
-  }
+//
   let dateElement = document.querySelector("#date");
   let currentTime = new Date();
   dateElement.innerHTML = formatDate(currentTime);
@@ -60,16 +60,6 @@ function formatDate(date) {
    
       //geolocalisation API
   
-    function displayWeatherCondition(response) {
-      document.querySelector("#city").innerHTML = response.data.name;
-      document.querySelector("#temperature").innerHTML = Math.round(
-        response.data.main.temp
-      );
-    
-     
-      document.querySelector("#description").innerHTML =
-        response.data.weather[0].main;
-    }
     
     function searchCity(city) {
       let units = "metric";
