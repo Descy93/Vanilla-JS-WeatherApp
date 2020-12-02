ef06f96a1e72eee733b969f31120f28c7200c074
Descy93 <74730041+Descy93@users.noreply.github.com>
2020-12-02 16:28:39 +0100

    Modification background

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ function formatDate(timestamp){
     let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
-      }
+    }
 
     let days = [
         "Sunday",
@@ -19,10 +19,8 @@ function formatDate(timestamp){
         "Friday",
         "Saturday"
       ];
-    let currentTime = new Date();
-    let day = days[date.getDay()];
-    dateElement.innerHTML = formatDate(currentTime);
-    return `${day} ${hours}:${minutes}`;
+      let day = days[date.getDay()];
+      return `${day} ${hours}:${minutes}`;
 }
 //general API
 function displayTemperature(response){
@@ -42,35 +40,34 @@ function displayTemperature(response){
     dateElement.innerHTML = formatDate(response.data.dt * 1000);  
 }
 
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let city= "Saintes";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-let url = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${apiKey}&units=${globalUnits}`;
-axios.get(apiUrl).then(displayTemperature);
-
 //
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
  
   function handleSubmit(event) {
     event.preventDefault();
     let city = document.querySelector("#city-input").value;
     searchCity(city);
   }
-  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
+
   function searchLocation(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     let units = "metric";
-    let form = document.querySelector("#search-form");
-  form.addEventListener("submit", handleSubmit);
-    searchCity("Saintes");
-    let currentLocationButton = document.querySelector("#current-location-button");
-  currentLocationButton.addEventListener("click", getCurrentLocation);
-    }
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
   
-  
-  
-  function getCurrentLocation(event) {
+   function getCurrentLocation(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(searchLocation);
   }
+  let currentLocationButton = document.querySelector("#current-location-button");
+ currentLocationButton.addEventListener("click", getCurrentLocation);
+
